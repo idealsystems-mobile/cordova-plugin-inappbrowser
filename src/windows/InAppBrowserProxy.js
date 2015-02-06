0dbf1d12d1abecbb1862e411d55b753466df5c66
TomVanBoghout <tvboghout@gmail.com>
2015-02-06 12:02:51 +0100

    Updated plugin

--- src/windows/InAppBrowserProxy.js
+++ src/windows/InAppBrowserProxy.js
@@ -1,4 +1,4 @@
-cordova.define("org.apache.cordova.inappbrowser.InAppBrowserProxy", function(require, exports, module) { /*
+/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -259,4 +259,3 @@ var IAB = {
 module.exports = IAB;
 
 require("cordova/exec/proxy").add("InAppBrowser", module.exports);
-});
